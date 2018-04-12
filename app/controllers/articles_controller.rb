126344024b634ea23b7376b9684c0f2d664b720d
Nam Hoon <namh5176@naver.com>
2018-04-12 12:16:14 +0000

    first commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,16 @@
+class ArticlesController < ApplicationController
+    def new
+    end
+    
+    def create
+        params.permit!
+        @article = Article.new(params[:article])
+        
+        @article.save()
+        redirect_to @article
+    end
+    
+    def show
+        @article = Article.find(params[:id])
+    end
+end

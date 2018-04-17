6645c1886de03942fbe781fbdc4b8f94a9bb012e
Nam Hoon <namh5176@naver.com>
2018-04-17 11:18:17 +0000

    fifth_class

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,7 +5,6 @@ class ArticlesController < ApplicationController
     def create
         params.permit!
         @article = Article.new(params[:article])
-        
         @article.save()
         redirect_to @article
     end
@@ -13,5 +12,11 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
         # test comment
+    end
+    
+    def index
+        @articles = Article.all
+    end
+    def destroy
     end
 end
